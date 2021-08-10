6c50336e032355583d5513928aad843bf9f058a2
Justin Byrne <theskaterjesus@gmail.com>
2021-08-10 18:20:53 +0100

    Added custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          'very-dark-desaturated': 'hsl(238, 29%, 16%)',
+          soft: 'hsl(240, 73%, 65%)',
+          'very-dark-grayish': 'hsl(237, 12%, 33%)',
+          'dark-grayish': 'hsl(240, 6%, 50%)',
+          'light-grayish': 'hsl(240, 5%, 91%)',
+        },
+        red: {
+          soft: 'hsl(14, 88%, 65%)',
+        },
+        violet: {
+          soft: 'hsl(273, 75%, 66%)',
+        },
+      },
+      fontFamily: {
+        'kumbh-sans': 'Kumbh Sans, sans-serif',
+      },
+    },
   },
   variants: {
     extend: {},
